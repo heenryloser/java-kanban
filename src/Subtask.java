f2d02f3d9d10a0e009dddaef2a28ad3b07f3265d
heenryloser <146265938+heenryloser@users.noreply.github.com>
2024-03-23 03:17:03 +0300

    Delete src/Subtask.java

--- src/Subtask.java
+++
@@ -1,5 +0,0 @@
-public class Subtask extends Task {
-    public Subtask(int taskId, String title, String description, TaskStatus status) {
-        super(taskId, title, description, status);
-    }
-}
